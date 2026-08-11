a205027cb7b5d066027bd3d81ab6178f62926bf5
LiamSDK <liamsdk99@gmail.com>
2026-08-11 19:01:15 -0300

    sea: Initial V816.0.16.0.UHZMIXM

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),sea)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,71a1b2aabffaad30a0ca0bb500fb0622d3054c15)
+$(call add-radio-file-sha1-checked,radio/gz.img,0dcea323d9f16fd4618b44397d1913a21f52bbc0)
+$(call add-radio-file-sha1-checked,radio/lk.img,0b716f19d42e08b2865a3d8be538afde1e775031)
+$(call add-radio-file-sha1-checked,radio/logo.img,8e140330350ec996049085d9e81f67bbbddd266f)
+$(call add-radio-file-sha1-checked,radio/md1img.img,cb25c03224a1d45603bc4a0ed2cb84b7da2b026d)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,31c8a22bea4c1f538e539840707691d42c57b546)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,df4bb64c1a462ec13954ae54714022cb5bc76736)
+$(call add-radio-file-sha1-checked,radio/scp.img,f9a34afed9b6345a3d1faaee9e1b9fc0c8297f26)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,6f9e228bad237c8756188efb0972236c76a3d1a4)
+$(call add-radio-file-sha1-checked,radio/sspm.img,fc319b6f1d33fe5f6134d4e1cd435d223e6f45b3)
+$(call add-radio-file-sha1-checked,radio/tee.img,f5b7f3ad7de34427e1505d77d300b1dab3c14220)
+
+endif
